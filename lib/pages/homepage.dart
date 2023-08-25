6e6adf3e7e583a5407f70935fada10f4641b5b87
wuvs999 <sujanmagar05825@gmail.com>
2023-08-25 08:59:46 +0545

    chang it

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -10,16 +10,58 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: appBar(),
       body: Column(children: [
-        Container(
-          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-          child: TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
+        SearchBar(),
+      ]),
+    );
+  }
+
+  Container SearchBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: const Color(0xff1d1617).withOpacity(0.11),
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'Search Panncake',
+          hintStyle: TextStyle(
+              color: Colors.amber, fontSize: 17, fontWeight: FontWeight.bold),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12),
+            child: SvgPicture.asset('assets/icons/search.svg'),
+          ),
+          suffixIcon: Container(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const VerticalDivider(
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset('assets/icons/filter.svg'),
+                  ),
+                ],
+              ),
             ),
           ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
         ),
-      ]),
+      ),
     );
   }
 
@@ -39,16 +81,16 @@ class HomePage extends StatelessWidget {
       leading: GestureDetector(
         onTap: () {},
         child: Container(
-          margin: EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
-          child: SvgPicture.asset(
-            'assets/icons/arrow.svg',
-            height: 20,
-            width: 20,
-          ),
           decoration: BoxDecoration(
             color: Colors.blueGrey,
             borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            "assets/icons/arrow.svg",
+            height: 20,
+            width: 20,
           ),
         ),
       ),
@@ -56,16 +98,16 @@ class HomePage extends StatelessWidget {
         GestureDetector(
           onTap: () {},
           child: Container(
-            margin: EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: SvgPicture.asset(
               'assets/icons/arrow.svg',
               height: 20,
               width: 20,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blueGrey,
-              borderRadius: BorderRadius.circular(10),
             ),
           ),
         ),
